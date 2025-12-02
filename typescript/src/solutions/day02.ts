c9392a2d463f27747d84a8c5ee95ab9f06bc00f4
Mathieu Post <mathieupost@gmail.com>
2025-12-02 10:31:49 +0100

    2025 day 2

---
+++ typescript/src/solutions/day02.ts
@@ -0,0 +1,60 @@
+import * as FileSystem from "@effect/platform/FileSystem"
+import * as Console from "effect/Console"
+import * as Effect from "effect/Effect"
+import { pipe } from "effect/Function"
+import * as Schema from "effect/Schema"
+import * as Stream from "effect/Stream"
+
+export default function run() {
+  return Effect.gen(function*() {
+    const fs = yield* FileSystem.FileSystem
+
+    const rangeSchema = Schema.TemplateLiteralParser(
+      Schema.Int,
+      "-",
+      Schema.Int
+    )
+
+    const ranges = pipe(
+      fs.stream("inputs/2025-day02.txt"),
+      Stream.decodeText("utf-8"),
+      Stream.splitLines,
+      Stream.flatMap((line: string) => Stream.fromIterable(line.split(","))),
+      Stream.map((range: string) => Schema.decodeUnknownSync(rangeSchema)(range)),
+      Stream.flatMap((range) => Stream.range(range[0], range[2]))
+    )
+
+    const resultPart1 = yield* ranges.pipe(
+      Stream.map(String),
+      Stream.filter((x) => x.length % 2 === 0),
+      Stream.filter((x) => x.slice(0, x.length / 2) === x.slice(x.length / 2)),
+      Stream.map(Number),
+      Stream.runSum
+    )
+
+    const resultPart2 = yield* ranges.pipe(
+      Stream.map(String),
+      Stream.filter((x) => {
+        outer: for (let i = Math.floor(x.length / 2); i > 0; i--) {
+          if (x.length % i !== 0) {
+            continue
+          }
+          const needle = x.slice(0, i)
+          for (let j = i; j < x.length; j += i) {
+            if (needle !== x.slice(j, j + i)) {
+              continue outer
+            }
+          }
+          return true
+        }
+        return false
+      }),
+      Stream.map(Number),
+      Stream.runSum
+    )
+
+    yield* Console.log("Day 2 Solution")
+    yield* Console.log("Part 1: ", resultPart1)
+    yield* Console.log("Part 2: ", resultPart2)
+  })
+}
--- typescript/src/solutions/index.ts
+++ typescript/src/solutions/index.ts
@@ -1,5 +1,7 @@
 import day01 from "./day01.js"
+import day02 from "./day02.js"
 
 export const solutions = {
-  1: day01
+  1: day01,
+  2: day02
 }
